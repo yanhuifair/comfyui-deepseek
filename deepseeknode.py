aa70d39a5d4541c73121cbd406177c97f27b3e36
Fair <541077827@qq.com>
2025-01-25 15:30:49 +0800

    DeepSeekReasonerNode

--- deepseeknode.py
+++ deepseeknode.py
@@ -1,5 +1,5 @@
-from openai import OpenAI
 import os
+from openai import OpenAI
 
 
 class DeepSeekChatNode:
@@ -16,7 +16,7 @@ class DeepSeekChatNode:
             "required": {
                 "prompt": ("STRING", {"multiline": True, "placeholder": "Type your prompt here"}),
                 "max_tokens": ("INT", {"default": "4096", "min": 1, "max": 8192}),
-                "temperature": ("FLOAT", {"default": "1", "min": 0, "max": 2}),
+                "temperature": ("FLOAT", {"default": "1", "min": 0, "max": 2, "tooltip": "创造性（越大越有创意，越小越严谨）"}),
             }
         }
 
@@ -51,11 +51,14 @@ class DeepSeekReasonerNode:
         with open(api_key_file_path, "r") as file:
             self.api_key = file.read()
 
+        self.history_content = []
+
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {
                 "prompt": ("STRING", {"multiline": True, "placeholder": "Type your prompt here"}),
+                "clear_history_content": ("BOOLEAN", {"default": False}),
                 "max_tokens": ("INT", {"default": "4096", "min": 1, "max": 8192}),
             }
         }
@@ -66,18 +69,23 @@ class DeepSeekReasonerNode:
 
     FUNCTION = "node_function"
 
-    def node_function(self, prompt, max_tokens):
+    def node_function(self, prompt, clear_history_content, max_tokens):
+        if clear_history_content:
+            self.history_content = []
+
+        self.history_content.append({"role": "user", "content": prompt})
+
         client = OpenAI(api_key=self.api_key, base_url="https://api.deepseek.com")
         response = client.chat.completions.create(
             model="deepseek-reasoner",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant"},
-                {"role": "user", "content": prompt},
-            ],
+            messages=self.history_content,
             stream=False,
             max_tokens=max_tokens,
         )
 
         reasoning_content = response.choices[0].message.reasoning_content
         content = response.choices[0].message.content
+
+        self.history_content.append({"role": "assistant", "content": content})
+
         return (reasoning_content, content)
